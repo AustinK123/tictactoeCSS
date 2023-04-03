79e033193d71c88a7e061f23b176ac637ce6a84f
austinK123 <austin_kinnealey23@milton.edu>
2023-04-03 14:12:53 -0400

    I made the X and O's alternate and added the array. continuing on the tutorial rn

--- src/App.js
+++ src/App.js
@@ -1,5 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+
+var XorO = 0
+
+function Square({value, onSquareClick}){
+
+  return <button className="square"> onClick={onSquareClick}
+    {value}
+    </button>;
+
+
+//const [value, setValue] = useState(null);
+
+// console.log('boom')
+
+//   function handleClick(){
+
+
+//     console.log("ouch you hit me");
+//     if(XorO === 0){
+//       setValue('X');
+
+// XorO = 1
+//     }
+//     else{
+//       setValue('O');
+//       XorO = 0
+//   }
+//   }
+//   console.log()
+  
+//   return <button className="square" onClick={handleClick}>{value}</button>
+}
+
+export default function Board(){
+  
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  
+return(<>
+
+<div className="board-row">
+  <Square value={squares[0]}/>
+  <Square value={squares[1]}/>
+  <Square value={squares[2]}/>
+</div>
+
+<div className="board-row">
+  <Square value={squares[3]}/>
+  <Square value={squares[4]}/>
+  <Square value={squares[5]}/>
+</div>
+
+<div className="board-row">
+  <Square value={squares[6]}/>
+  <Square value={squares[7]}/>
+  <Square value={squares[8]}/>
+</div>
+
+    </>
+  );
+}
 
 function App() {
   return (
@@ -22,4 +83,4 @@ function App() {
   );
 }
 
-export default App;
+//export default App;
